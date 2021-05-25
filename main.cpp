178ba97a50bf291fdaffee98943c46856e2509e4
Dave-Avans <dl.visser@student.avans.nl>
2021-05-25 14:12:19 +0200

    [Add]: modelLoader

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "tigl.h"
+#include "ObjModel.h"
 #include <glm/gtc/matrix_transform.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <iostream>
-
-#include <map>
-
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
-#include "Scene.h"
-#include "StartupScene.h"
-#include "SceneIngame.h"
-
 using tigl::Vertex;
 
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "opengl32.lib")
 
-using namespace cv;
-using namespace std;
-
 GLFWwindow* window;
+ObjModel* model;
 
 void init();
 void update();
@@ -63,9 +47,6 @@ int main(void)
 }
 
 
-std::map<Scenes, Scene*> scenes;
-Scene* currentScene = nullptr;
-
 void init()
 {
     glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -74,16 +55,15 @@ void init()
             glfwSetWindowShouldClose(window, true);
     });
 
-    //scenes[Scenes::STARTUP] = new StartupScene();
-    //scenes[Scenes::INGAME] = new SceneIngame();
-    //currentScene = scenes[Scenes::STARTUP];
 
+    model = new ObjModel("models/word_raiders/Cube_Word_Raiders.obj");
 }
 
+float rotation = 0;
 
 void update()
 {
-    //currentScene->update();
+    rotation += 0.01f;
 }
 
 void draw()
@@ -91,16 +71,19 @@ void draw()
     glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //ImGui_ImplOpenGL3_NewFrame();
-    //ImGui_ImplGlfw_NewFrame();
-    //ImGui::NewFrame();
+    int viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    glm::mat4 projection = glm::perspective(glm::radians(75.0f), viewport[2] / (float)viewport[3], 0.01f, 500.0f);
+
+    tigl::shader->setProjectionMatrix(projection);
+    tigl::shader->setViewMatrix(glm::lookAt(glm::vec3(0,5.0f,10.0f), glm::vec3(0,0,0), glm::vec3(0,1,0)));
+    tigl::shader->setModelMatrix(glm::rotate(glm::mat4(1.0f), rotation, glm::vec3(0,1,0)));
+   
+
+    tigl::shader->enableColor(true);
+
+    glEnable(GL_DEPTH_TEST);
 
 
-    //currentScene->draw();
-
-    //ImGui::ShowDemoWindow();
-
-
-    //ImGui::Render();
-    //ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    model->draw();
 }
