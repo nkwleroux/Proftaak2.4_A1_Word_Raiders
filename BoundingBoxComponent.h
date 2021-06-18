ff65485b882dd0234857ac5c09b72352d1446def
Davy <dj.raitt@student.avans.nl>
2021-06-18 10:54:52 +0200

    [PUSH] Done with BoundingBoxComponent

--- BoundingBoxComponent.h
+++ BoundingBoxComponent.h
@@ -1,22 +1,32 @@
 #pragma once
-#include "tigl.h"
+//#include "tigl.h"
 #include "Component.h"
 #include "DrawComponent.h"
-
 #include <vector>
+
 class BoundingBoxComponent : public DrawComponent
 {
 
 public:
-	glm::vec3 min, max;
+	// Minimum and maximum lenght of the bounding box component
+	glm::vec3 minBoundingBox, maxBoundingBox;
 
+	// Set collision on false, because there is none yet
 	bool collisionX = false, collisionY = false, collisionZ = false;
 
+	// Defining the constructor
 	BoundingBoxComponent(GameObject* gameObject);
+
+	// Defining the destructor
 	~BoundingBoxComponent();
+
+	// Add a vector component
 	void addVector(glm::vec3 vector);
 
+	//Boolean for colliding with another object
 	bool collideWithObject(GameObject* otherObject);
+
+	//Boolean for colliding with the skybox wall
 	bool collideWithWall(GameObject* otherObject);
 
 	//virtual void setTexture(Texture* t) override;
