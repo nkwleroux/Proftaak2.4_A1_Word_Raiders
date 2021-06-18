2248fa9c5974bf7d8a49215e873363cb6bb61352
Davy <dj.raitt@student.avans.nl>
2021-06-18 11:32:34 +0200

    [PUSH] Done with PlayerComponent c and header file

--- PlayerComponent.h
+++ PlayerComponent.h
@@ -4,11 +4,14 @@
 
 class PlayerComponent : public Component
 {
+	// Moving speed is 5
 	float speed = 5;
 public:
+	//Constructor
 	PlayerComponent();
+	//Destructor
 	~PlayerComponent();
-
+	// Update function for PlayerComponent
 	virtual void update(float elapsedTime) override;
 };
 
