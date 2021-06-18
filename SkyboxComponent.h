d7e53d9a0a0838c418016ce66d310fb2430fc63c
Davy <dj.raitt@student.avans.nl>
2021-06-18 11:42:58 +0200

    [PUSH] Done with SkyboxComponent c and header file

--- SkyboxComponent.h
+++ SkyboxComponent.h
@@ -3,23 +3,30 @@
 #include "DrawComponent.h"
 #include "tigl.h"
 #include "Texture.h"
-using tigl::Vertex;
-
 #include <vector>
+
+// Using to make things a bit easier
+using tigl::Vertex;
 
 class SkyboxComponent : public DrawComponent
 {
-	std::vector<std::vector<Vertex>> verts;
+	// Variable for list of vertexes
+	std::vector<std::vector<Vertex>> vertexList;
+	// Variables for x y and z axis
 	float x = 0, y = 0, z = 0;
 
 public:
+	// Constructor in which we can pass size and textures.
 	SkyboxComponent(float size, Texture** textures);
+	// 	Constructor in which we can pass size and textures and the length!
 	SkyboxComponent(float height, float width, float length, Texture** textures);
-
+	// Destructor
 	~SkyboxComponent();
 
+	// Textures variable
 	Texture** textures;
 
+	// Draw function for the SkyboxComponent
 	virtual void draw() override;
 
 	// Inherited via DrawComponent
