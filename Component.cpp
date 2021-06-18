9ce14257b6a1cec53b24136ff22f78c29bdc0159
Davy <dj.raitt@student.avans.nl>
2021-06-18 10:57:25 +0200

    [PUSH] Done with Component

--- Component.cpp
+++ Component.cpp
@@ -1,12 +1,12 @@
 #include "Component.h"
 
 
-
+//Constructor
 Component::Component()
 {
 }
 
-
+//Destructor
 Component::~Component()
 {
 }
--- Component.h
+++ Component.h
@@ -7,11 +7,16 @@ class Component
 protected:
 	GameObject* gameObject;
 public:
+	//Constructor
 	Component();
+
+	//Destructor
 	~Component();
 
+	//Update function
 	virtual void update(float elapsedTime) {};
 
+	//Set game object
 	inline void setGameObject(GameObject* gameObject) { this->gameObject = gameObject; }
 };
 
